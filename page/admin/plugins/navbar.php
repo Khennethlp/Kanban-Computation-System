c52526d7bf7dc6583af1108dd3717516f15bed63
khenneth <khennethlp@gmail.com>
2024-12-18 17:24:53 +0800

    download font family

--- page/admin/plugins/navbar.php
+++ page/admin/plugins/navbar.php
@@ -23,11 +23,12 @@ if (!isset($_SESSION['username'])) {
   <!-- Google Font Nunito Sans / Poppins / Montserrat -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> -->
  
   <link rel="stylesheet" href="../../plugins/DataTables/datatables.min.css">
   <link rel="stylesheet" href="../../dist/css/datatable/dataTables.dataTables.min.css">
   <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/fontfamily.css">
   <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
