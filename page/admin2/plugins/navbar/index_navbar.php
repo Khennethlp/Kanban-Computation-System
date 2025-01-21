a210b3fef42acb9e764ed4c3ee36ac398df112e9
khenneth <khennethlp@gmail.com>
2025-01-21 10:26:29 +0800

    changed navbar

--- page/admin2/plugins/navbar/index_navbar.php
+++ page/admin2/plugins/navbar/index_navbar.php
@@ -1,85 +1,46 @@
-<!-- Navbar -->
-<nav class="main-header navbar navbar-expand-md navbar-light bg-white text-dark border-bottom-0 elevation-1 pt-3 pb-3">
-  <div class="col-md-12">
-    <div class="row">
-      <div class="col-md-3">
-        <a href="" class="navbar-brand ml-2">
-          <img src="../../dist/img/kcs-bg.webp" alt="Web Template Logo" class="brand-image elevation-1 bg-light p-1 rounded" style="opacity: .8">
-          <span class="brand-text font-weight-bold text-dark text-lg">Kanban Computation</span>
-        </a>
-      </div>
-    
-
-
-    <div class="col-md-7">
-      <div class="row">
-        <div class="col-md-12">
-          <!-- Left navbar links -->
-          <ul class="navbar-nav ">
-            <li class="nav-item">
-              <?php if ($_SERVER['REQUEST_URI'] == "/kcs/page/admin2/index.php") { ?>
-                <a href="index.php" class="nav-link active">
-                <?php } else { ?>
-                  <a href="index.php" class="nav-link">
-                  <?php } ?>
-                  <i class="nav-icon fa fa-home"></i>
-                  Overview
-                  </a>
-            </li>
-            <li class="nav-item">
-              <?php if ($_SERVER['REQUEST_URI'] == "/kcs/page/admin2/masterlist.php") { ?>
-                <a href="masterlist.php" class="nav-link active">
-                <?php } else { ?>
-                  <a href="masterlist.php" class="nav-link">
-                  <?php } ?>
-                  <i class="nav-icon fa fa-table"></i>
-                  Masterlist
-                  </a>
-            </li>
-
-            <li class="nav-item">
-              <?php if ($_SERVER['REQUEST_URI'] == "/kcs/page/admin2/master_combine.php") { ?>
-                <a href="master_combine.php" class="nav-link active">
-                <?php } else { ?>
-                  <a href="master_combine.php" class="nav-link">
-                  <?php } ?>
-                  <i class="nav-icon fa fa-file-alt"></i>
-                  Bom Combine
-                  </a>
-            </li>
-            <li class="nav-item">
-              <?php if ($_SERVER['REQUEST_URI'] == "/kcs/page/admin2/accounts.php") { ?>
-                <a href="accounts.php" class="nav-link active">
-                <?php } else { ?>
-                  <a href="accounts.php" class="nav-link">
-                  <?php } ?>
-                  <i class="nav-icon fa fa-user-cog"></i>
-                  Accounts
-                  </a>
-            </li>
-            <li class="nav-item">
-              <?php if ($_SERVER['REQUEST_URI'] == "/kcs/page/admin2/others.php") { ?>
-                <a href="others.php" class="nav-link active">
-                <?php } else { ?>
-                  <a href="others.php" class="nav-link">
-                  <?php } ?>
-                  <i class="nav-icon fa fa-cog"></i>
-                  Others
-                  </a>
-            </li>
-            <li class="nav-item">
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-2 ml-auto">
-      <div class="col-md-6 ml-auto">
-        <a href="#" class="text-dark border rounded btn btn-md " data-toggle="modal" data-target="#logout_modal"><i class="fas fa-sign-out-alt"></i> Logout</a>
-      </div>
-    </div>
-    </div>
+<!-- Main Navbar -->
+<nav class="main-header navbar navbar-expand-md navbar-light bg-white text-dark border-0 pt-2 pb-2">
+  <div class="d-flex justify-content-between align-items-center w-100">
+    <!-- Brand -->
+    <a href="" class="navbar-brand ml-2">
+      <img src="../../dist/img/kcs-bg.webp" alt="Web Template Logo" class="brand-image elevation-1 bg-light p-0 rounded" style="opacity: .8">
+      <span class="brand-text font-weight-bold text-dark text-lg">Kanban Computation</span>
+    </a>
+    <!-- Logout Button -->
+    <a href="#" class="text-white border rounded btn btn-md btn-dark" data-toggle="modal" data-target="#logout_modal">
+      <i class="fas fa-sign-out-alt"></i> Logout
+    </a>
   </div>
 </nav>
-<!-- /.navbar -->
+
+<nav class="main-header navbar navbar-expand-md navbar-light bg-white text-dark border-bottom-1 elevation-0 pt-2 pb-2 sticky-top">
+  <div class="container">
+    <ul class="navbar-nav w-100 d-flex justify-content-around">
+      <li class="nav-item">
+        <a href="index.php" class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/kcs/page/admin2/index.php') ? 'active' : '' ?>">
+          <i class="nav-icon fa fa-home"></i> Overview
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="masterlist.php" class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/kcs/page/admin2/masterlist.php') ? 'active' : '' ?>">
+          <i class="nav-icon fa fa-table"></i> Masterlist
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="master_combine.php" class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/kcs/page/admin2/master_combine.php') ? 'active' : '' ?>">
+          <i class="nav-icon fa fa-file-alt"></i> Bom Combine
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="accounts.php" class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/kcs/page/admin2/accounts.php') ? 'active' : '' ?>">
+          <i class="nav-icon fa fa-user-cog"></i> Accounts
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="others.php" class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/kcs/page/admin2/others.php') ? 'active' : '' ?>">
+          <i class="nav-icon fa fa-cog"></i> Others
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
