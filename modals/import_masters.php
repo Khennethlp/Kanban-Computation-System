f80f07d9cc3eb4237b8863fab60bade14f9dd18a
khenneth <khennethlp@gmail.com>
2025-01-25 09:55:41 +0800

    changed template layout

--- modals/import_masters.php
+++ modals/import_masters.php
@@ -12,35 +12,30 @@
                     <form id="csvFileForms" method="post" enctype="multipart/form-data">
                         <div class="row">
                             <input type="hidden" id="userName" class="form-control mb-2 mt-2" value="<?= $_SESSION['name']; ?>">
-                            <div class="col-md-8 mb-2">
+
+                            <div class="col-md-12 mb-2">
                                 <label for="">Max Plan:</label>
                                 <input type="file" class="form-control p-1" name="file" accept=".csv, .xls, .xlsx" id="csvFileInput_maxplan">
+                                <span><a href="../../template/Max Plan.csv" class="text-info text-sm ">Download Max Plan Template</a></span>
                             </div>
-                            <div class="col-md-4 ">
-                                <label for="">&nbsp;</label>
-                                <a href="../../template/Max Plan.csv" class="form-control text-dark text-center mb-2"><i class="fas fa-download"></i> Template</a>
-                            </div>
-                            <div class="col-md-8 mb-2">
+
+                            <div class="col-md-12 mb-2">
                                 <label for="">Min Lot:</label>
                                 <input type="file" class="form-control p-1" name="file" accept=".csv, .xls, .xlsx" id="csvFileInput_minlot">
+                                <span><a href="../../template/Minlot.csv" class="text-info text-sm">Download Minlot Template</a></span>
                             </div>
-                            <div class="col-md-4">
-                                <label for="">&nbsp;</label>
-                                <a href="../../template/Minlot.csv" class="form-control text-dark text-center mb-2"><i class="fas fa-download"></i> Template</a>
-                            </div>
-                            <div class="col-md-8 mb-2">
+
+                            <div class="col-md-12 mb-2">
                                 <label for="">No. of teams:</label>
                                 <input type="file" class="form-control p-1" name="file" accept=".csv, .xls, .xlsx" id="csvFileInput_teams">
+                                <span><a href="../../template/No of Teams.csv" class="text-info text-sm">Download No. of Teams Template</a></span>
                             </div>
-                            <div class="col-md-4">
-                                <label for="">&nbsp;</label>
-                                <a href="../../template/No of Teams.csv" class="form-control text-dark text-center mb-2"><i class="fas fa-download"></i> Template</a>
-                            </div>
-                            <div class="col-md-8 mb-2">
+
+                            <div class="col-md-12 mb-2">
                                 <label for="">Kanban Masterlist:</label>
                                 <input type="file" class="form-control p-1" name="file" accept=".csv, .xls, .xlsx" id="csvFileInput_kanban">
                             </div>
-                            <input type="submit" name="upload" class="form-control mt-3  btn-success" value="Upload">
+                            <input type="submit" name="upload" class="form-control mt-3  activeBtn" value="Upload">
                         </div>
                     </form>
                 </div>
